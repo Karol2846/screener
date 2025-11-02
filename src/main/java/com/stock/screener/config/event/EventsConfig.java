c63ce4116587b36e9c89074ce8eb18ffb81f0822
Karol2846 <karol200242@gmail.com>
2025-11-02 17:47:25 +0100

    scheduling is performed automatically

--- src/main/java/com/stock/screener/config/event/EventsConfig.java
+++ src/main/java/com/stock/screener/config/event/EventsConfig.java
@@ -1,9 +1,19 @@
 package com.stock.screener.config.event;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
 public class EventsConfig {
+
+    @Bean
+    @Primary
+    public TaskExecutor asynctaskExecutor() {
+        return new ThreadPoolTaskExecutor();
+    }
 }
---
+++ src/main/java/com/stock/screener/config/schedule/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.stock.screener.config.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
